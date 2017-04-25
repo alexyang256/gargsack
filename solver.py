da5a5a6af520acd23464fd873dcd6e51a7db6830
Bryan Lov <bryan_lov@yahoo.com>
2017-04-24 22:49:28 -0700

    working on solver for knapsack

--- solver.py
+++ solver.py
@@ -3,29 +3,40 @@
 from __future__ import division
 import argparse
 import random
+import numpy as np
+import scipy.io as io 
 
 """
 ===============================================================================
   Please complete the following function.
 ===============================================================================
 """
-def writeGraph(item_constraints, filename):
+def writeGraph(item_constraints, filename, id):
   file = open(filename, 'w')
   for item, incompatible in item_constraints.items():
     toWrite = str((item, incompatible))+'\n'
     file.write(toWrite)
   file.close()
 
-def solve(P, M, N, C, items, constraints):
+#####KNAPSACK SOLVER#####
+def knapsackSolver(items, P, M):
+  solution = []
+  knapsack_matrix = np.zeros((len(items), P, M))
+  for i in range(len(items)):
+    for w in range(P):
+      for c in range(M):
+        items[i][2]
+
+def generateMatrices(id, P, M, N, C, items, constraints):
   """
   Write your amazing algorithm here.
 
   Return: a list of strings, corresponding to item names.
   """
+  graphDest = "problem_graphs/problem" + str(id) + ".mat"
   itemscopy = list(items)
-  items = set()
+
   for item in itemscopy:
-    items.add(item)
     if item[4] - item[3] <= 0 or item[2] > P or item[3] > M:
       items.remove(item)
 
@@ -48,7 +59,7 @@ def solve(P, M, N, C, items, constraints):
         for incompatible in incompatibles:
           class_constraint_map[clas].add(incompatible)
     count += 1
-
+  """
   # Adj list items
   item_constraint_map = dict()
   for item in items:
@@ -59,11 +70,24 @@ def solve(P, M, N, C, items, constraints):
     for item2 in items:
       if item2[1] in class_constraint_map[item[1]]:
         item_constraint_map[item[0]].add(item2[0])
-  writeGraph(item_constraint_map, "problem_graphs/problem1.graph")
+  writeGraph(item_constraint_map, "problem_graphs/problem3.graph")
   solution = []
   print(min(items, key=lambda item: item[3]))
+  """
   iterations = 0
+  
+  item_matrix = np.zeros((len(items), len(items)))
+  for i in range(len(items)):
+    for j in range(i):
+      if items[j][1] in class_constraint_map[items[i][1]]:
+        item_matrix[i][j] = 1
+        item_matrix[j][i] = 1
+
+  io.savemat(graphDest, {"graph": item_matrix})
+
   return []
+
+"""
   while iterations < len(items) and len(items) > 0 and P >= min(items, key=lambda item: item[2])[2] and M >= min(items, key=lambda item: item[3])[3]:
 
     item = max(items, key=lambda item: (item[4] - item[3])/max(0.01, item[2]))
@@ -79,6 +103,7 @@ def solve(P, M, N, C, items, constraints):
         items.remove(incompatible)
     iterations += 1
   return solution
+  """
 """
 ===============================================================================
   No need to change any code below this line.
@@ -119,8 +144,9 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="PickItems solver.")
   parser.add_argument("input_file", type=str, help="____.in")
   parser.add_argument("output_file", type=str, help="____.out")
+  parser.add_argument("id", type=str)
   args = parser.parse_args()
 
   P, M, N, C, items, constraints = read_input(args.input_file)
-  items_chosen = solve(P, M, N, C, items, constraints)
+  items_chosen = generateMatrices(args.id, P, M, N, C, items, constraints)
   write_output(args.output_file, items_chosen)
